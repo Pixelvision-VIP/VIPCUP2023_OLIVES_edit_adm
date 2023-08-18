0428441834a0a8871e2bd3855a1d55b4f45b0dc8
Deependra Dewagiri <56537975+deepdewdeep@users.noreply.github.com>
2023-08-19 00:30:02 +0530

    Update train.py

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ def main():
         opt.save_folder, 'last.pth')
     save_model(model, optimizer, opt, opt.epochs, save_file)
 
-    output_state('Res',1,1)
+    output_state(opt.model,1,1)
 
    # global output_csv_name='a'#str(model)#+str(opt.epochs)+str(opt.batch_size) #this line is added to output name for csv.use this vairable only.
 
