d27e21c1b55d595353612aace950e8dd68fc26e4
Deependra Dewagiri <56537975+deepdewdeep@users.noreply.github.com>
2023-08-20 17:08:02 +0530

    Update model.py

--- model.py
+++ model.py
@@ -14,6 +14,12 @@ class ResNet(nn.Module):
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
 
+        elif(name == 'resnet34'):##dd
+            self.encoder = torchvision.models.resnet101(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(512, num_classes)
+
         elif(name == 'resnet101'):##dd
             self.encoder = torchvision.models.resnet101(zero_init_residual=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
